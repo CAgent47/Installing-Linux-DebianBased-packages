94ff8c882473f2b4d45b1c0eb82130c0a01eca90
CAgent47 <mpgbieghesh468@gmail.com>
2026-08-14 15:51:33 +0330

    created python madules file Features: Get user Status(Root Or Usr), Create Json File if not exists, detect pkg manager, reading json file for detect required command

---
+++ dockerfile/Dockerimage/engine-syntax/dockermadule.py
@@ -0,0 +1,19 @@
+import json, os, shutil
+
+
+def readJsonFile(file):
+    with open(file, 'r') as ReadJson:
+        return json.load(ReadJson)
+
+def detectSyntax(list):
+    for index, syntax in list.items():
+        if shutil.which(index):
+            return syntax
+
+def createJsonFile(file, list):
+    with open(file, 'w') as CJson:
+        json.dump(list, CJson, indent=4)
+
+def getCommand(manager, action):
+    mode = "root" if os.geteuid() == 0 else "sudo"
+    return manager[mode][action]
