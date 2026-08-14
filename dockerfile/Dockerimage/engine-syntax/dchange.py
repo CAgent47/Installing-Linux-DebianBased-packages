b81606aa2c2915377c8854186108479338d98042
CAgent47 <mpgbieghesh468@gmail.com>
2026-08-14 17:35:37 +0330

    printing appropriate compose syntax

---
+++ dockerfile/Dockerimage/engine-syntax/dchange.py
@@ -0,0 +1,28 @@
+import dockermadule
+
+Dockerfile = """
+FROM debian:bookworm
+
+RUN apt-get update && apt-get install -y \
+    bash \
+    python3 \
+    jq \
+    curl \
+    git \
+    && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /omnipkg
+
+COPY . /omnipkg/
+
+RUN chmod +x omnipkg.sh
+
+CMD ["./omnipkg.sh"]
+"""
+
+check_change = dockermadule.readDockerfile('docker-img/Dockerfile')
+if dockermadule.exists('docker-img/Dockerfile'):
+    if Dockerfile != check_change:
+        print("sudo docker compose up --build -d")
+    else:
+        print("sudo docker compose up -d")
