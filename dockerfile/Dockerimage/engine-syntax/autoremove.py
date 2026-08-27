731847d55d2bf030db4e6bd617ea0e616dff80d5
Dev-vesper <vesper.oneold@gmail.com>
2026-08-27 18:42:20 +0330

    same update for setup, auto and install repo...

--- dockerfile/Dockerimage/engine-syntax/autoremove.py
+++ dockerfile/Dockerimage/engine-syntax/autoremove.py
@@ -1,7 +1,15 @@
+import os
+import sys
 import dockermadule
 
-SyntaxInstall = dockermadule.readJsonFile('engine-syntax/dockerinstall.json')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+SyntaxInstall = dockermadule.readJsonFile(os.path.join(BASE_DIR, 'dockerinstall.json'))
 
 DetectSyntax = dockermadule.detectSyntax(SyntaxInstall)
+
+if DetectSyntax is None:
+    print("[ Docker-Engine ]: No supported package manager detected", file=sys.stderr)
+    sys.exit(1)
 
 print(dockermadule.getCommand(DetectSyntax, "autoremove"))
